0b9cd45bf52415d5379ec0e4d12a9ebce0f805b5
Remi <remi.blom-ohlsen@puzzlepart.com>
2020-02-06 16:07:50 +0100

    Finish form

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-(() => {
-    console.log(`
-    _______                                 __                                            __            _______   __  __  __                                                   
-   /       \\                              /  |                                           /  |          /       \\ /  |/  |/  |                                                  
-   $$$$$$$  | __    __  ________  ________ $$ |  ______    ______    ______    ______   _$$ |_         $$$$$$$  |$$/ $$ |$$ |  ______    ______    ______    ______    _______ 
-   $$ |__$$ |/  |  /  |/        |/        |$$ | /      \\  /      \\  /      \\ /       \\ / $$   |        $$ |__$$ |/  |$$ |$$ | /      \\  /      \\  /      \\  /      \\  /       |
-   $$    $$/ $$ |  $$ |$$$$$$$$/ $$$$$$$$/ $$ |/$$$$$$  |/$$$$$$  | $$$$$$  |/$$$$$$  |$$$$$$/         $$    $$/ $$ |$$ |$$ | $$$$$$  |/$$$$$$  |/$$$$$$  |/$$$$$$  |/$$$$$$$/ 
-   $$$$$$$/  $$ |  $$ |  /  $$/    /  $$/  $$ |$$    $$ |$$ |  $$ | /    $$ |$$ |  $$/   $$ | __       $$$$$$$/  $$ |$$ |$$ | /    $$ |$$ |  $$ |$$    $$ |$$ |  $$/ $$      \ 
-   $$ |      $$ \\__$$ | /$$$$/__  /$$$$/__ $$ |$$$$$$$$/ $$ |__$$ |/$$$$$$$ |$$ |        $$ |/  |      $$ |      $$ |$$ |$$ |/$$$$$$$ |$$ \__$$ |$$$$$$$$/ $$ |       $$$$$$  |
-   $$ |      $$    $$/ /$$      |/$$      |$$ |$$       |$$    $$/ $$    $$ |$$ |        $$  $$/       $$ |      $$ |$$ |$$ |$$    $$ |$$    $$ |$$       |$$ |      /     $$/ 
-   $$/        $$$$$$/  $$$$$$$$/ $$$$$$$$/ $$/  $$$$$$$/ $$$$$$$/   $$$$$$$/ $$/          $$$$/        $$/       $$/ $$/ $$/  $$$$$$$/  $$$$$$$ | $$$$$$$/ $$/       $$$$$$$/  
-                                                         $$ |                                                                          /  \\__$$ |                              
-                                                         $$ |                                                                          $$    $$/                               
-                                                         $$/                                                                            $$$$$$/                                
-   `)
- })()
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
