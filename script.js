55e448c1ae547d915c70c193f05ecec6efd44a1b
Remi <remi.blom-ohlsen@puzzlepart.com>
2020-02-06 16:45:37 +0100

    Add submit function for initialising flow from input

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+async function submitForm(event) {
+  var formEmailValue = event.target.elements.emailValue.value;
+  var formDisplayNameValue = event.target.elements.userNameValue.value;
+
+  bodyString = JSON.stringify({ email: formEmailValue, displayname: formDisplayNameValue })
+  
+  console.log("asdasd" + bodyString);
+
+  await fetch(`https://prod-86.westeurope.logic.azure.com:443/workflows/1e05bc32554344fc9c4a184579c0ce79/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=FFgFQoXn4DErW0bBk7l_nsUcROCK61mDehEuEwN_vyU`, {
+    method: "POST",
+    body: bodyString,
+  }).then(d => d.text().then(f => f))
+
+  console.log(bodyString);
+  
+  alert("Mail value: " + formEmailValue + "DisplayName value: " + formDisplayNameValue);
+};
+
+
 $('.form').find('input, textarea').on('keyup blur focus', function (e) {
   
   var $this = $(this),
